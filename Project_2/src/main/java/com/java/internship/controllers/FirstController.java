1fe84255b1728d8633244ff9c89e622b13b3eed7
Txao <txao@pm.me>
2021-02-22 21:10:05 +0300

    update

--- Project_2/src/main/java/com/java/internship/controllers/FirstController.java
+++ Project_2/src/main/java/com/java/internship/controllers/FirstController.java
@@ -1,17 +1,54 @@
 package com.java.internship.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
-    @GetMapping("/")
+    @GetMapping("/first")
     public String firstPage() {
         return "first/index";
     }
 
     @GetMapping("/second")
-    public String secondPage() {
+    public String secondPage(@RequestParam(value = "name", required = false) String name,
+                             @RequestParam(value = "surname", required = false) String surname,
+                             Model model) {
+
+        model.addAttribute("message", "Hello " + name + " " + surname);
+
         return "first/second";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("x") int x, @RequestParam("y") int y,
+                             @RequestParam("z") char z, Model model) {
+
+        int result = 0;
+
+        switch (z) {
+            case '+':
+                result = x + y;
+                break;
+
+            case '-':
+                result = x - y;
+                break;
+
+            case '*':
+                result = x * y;
+                break;
+
+            case '/':
+                result = x / y;
+                break;
+        }
+
+        model.addAttribute("result", "Result: " + result);
+        return "first/calculator";
     }
 }
